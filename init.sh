92894f4c5be29ad6b1216c49eeeddd9610b8caeb
Dave Burke <dburke84@gmail.com>
2017-01-04 14:24:20 -0600

    Initial commit.

---
+++ init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+IMAGE="plexinc/pms-docker:plexpass"
+CONTAINER="plex"
+
+docker run \
+--name "${CONTAINER}" \
+--detach \
+--restart=always \
+--publish 32400:32400/tcp \
+--publish 3005:3005/tcp \
+--publish 8324:8324/tcp \
+--publish 32469:32469/tcp \
+--publish 1900:1900/udp \
+--publish 32410:32410/udp \
+--publish 32412:32412/udp \
+--publish 32413:32413/udp \
+--publish 32414:32414/udp \
+--env TZ="America/Chicago" \
+--env ADVERTISE_IP="http://192.168.2.150:32400/" \
+--env PLEX_UID=111 \
+--env PLEX_GID=117 \
+--hostname plex.servy.lan \
+--volume /mnt/storage/var/lib/plexmediaserver:/config \
+--volume /mnt/storage/audio:/mnt/storage/audio \
+--volume /mnt/storage/videos:/mnt/storage/videos \
+--volume /mnt/storage/photos:/mnt/storage/photos \
+${IMAGE}
+
